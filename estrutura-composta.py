178da6842eeb27b6f0eb61dbaa1b57e8e9ee2f2a
KetlinCasagrande <Ketlin.casagrande010@gmail.com>
2024-02-22 19:46:36 -0300

    commit inicial

---
+++ estrutura-composta.py
@@ -0,0 +1,10 @@
+# variavel
+nota= 6
+
+# condição
+if nota >= 7:
+    print("aprovado!")
+elif nota >=6:
+    print("Recuperação")
+else:
+    print("Reprovado!")
---
+++ estruutura-simples.py
@@ -0,0 +1,7 @@
+# variavel
+idade = 19
+
+# condição
+
+if idade >= 18:
+    print("idade!")
